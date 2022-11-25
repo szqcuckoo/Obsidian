bb36990cba05041e0b5f7ec940d4a48fa3c1f428
szqcuckoo <szqcuckoo@163.com>
2022-11-25 17:37:24 +0800

    vault backup: 2022-11-25 17:37:23

--- 004-面试题/promise.js
+++ 004-面试题/promise.js
@@ -1,61 +1,28 @@
-// fetch(url, sosuccess, onfailed)
+const getHome = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("home数据")
+    }, 1000)
+  })
+const getList = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("list数据")
+    }, 1000)
+  })
+const getSide = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("side数据")
+    }, 1000)
+  })
 
-// Promise.all([
-//   new Promise((resolve, reject) => {
-//     fetch(
-//       url1,
-//       res => {
-//         console.log("res:", res)
-//       },
-//       err => {
-//         console.log("err:", err)
-//       }
-//     )
-//   }),
-//   new Promise((resolve, reject) => {
-//     fetch(
-//       url2,
-//       res => {
-//         console.log("res:", res)
-//       },
-//       err => {
-//         console.log("err:", err)
-//       }
-//     )
-//   }),
-// ]).then(function(res){
-//   console.log('load success:')
-// }, function(err) {
+const all = Promise.all([getHome(), getList(), getSide()])
 
-// })
-
-const p1 = new Promise((resolve, reject) => {
-  resolve(1)
-})
-const p2 = new Promise((resolve, reject) => {
-  resolve(2)
-})
-const p3 = new Promise((resolve, reject) => {
-  reject("err1")
-  // resolve(3)
-})
-const p4 = new Promise((resolve, reject) => {
-  reject("err2")
-})
-
-const pAll = Promise.all(
-  [p1, p2, p3, p4].map(p => 
-    p.catch(err => err)
-  )
-)
-
-pAll
+all
   .then(res => {
-    console.log("load success", res)
+    console.log("sucess")
   })
   .catch(err => {
     console.log("err:", err)
   })
-  // .finally(res => {
-  //   console.log("res:", res)
-  // })
